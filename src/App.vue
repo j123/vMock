3a75aa37c6ba36ef7bc8c3bcd1b466f790fa92e0
yasunori jitsukawa <j123death4ne@gmail.com>
2019-03-17 23:02:30 +0900

    Example of API Calls

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,67 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div class="root">
+    <Search class="search" @loadStart="onLoadStart" @loadComplete="onLoadComplete"/>
+    <Result :items="items" :loadProgress="loadProgress"/>
   </div>
 </template>
 
 <script>
+import Search from '@/components/Search'
+import Result from '@/components/Result'
+
 export default {
-  name: 'App'
+  data () {
+    return {
+      items: [],
+      loadProgress: false
+    }
+  },
+  methods: {
+    onLoadStart () {
+      this.loadProgress = true
+    },
+    onLoadComplete ({ results }) {
+      this.items = results
+      this.loadProgress = false
+    }
+  },
+  components: {
+    Search,
+    Result
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.root {
+  padding-top: 70px;
+}
+
+.search {
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1;
+}
+</style>
+
+<style>
+html {
+  font-size: 62.5%;
+}
+
+body {
+  margin: 0;
+  color: #35495e;
+}
+
+p {
+  margin: 0;
+}
+
+ul {
+  padding: 0;
+  margin: 0;
 }
 </style>
